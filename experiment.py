5b2b5c9f50357901c316953a761bdccdb11c49b5
James Draper <jame.draper@duke.edu>
2016-10-13 10:58:00 -0400

    Lots of documentation for experiment.py
    Trying to get this method completely annotated correctely.

--- experiment.py
+++ experiment.py
@@ -23,10 +23,10 @@ class RawData:
 
     Attributes
     ----------
-        peptides :  DataFrame
-            Raw data from Proteome Discoverer peptides data.
-        proteins : DataFrame
-            Raw data Proteome Discoverer corresponding proteins data.
+    peptides :  DataFrame
+        Raw data from Proteome Discoverer peptides data.
+    proteins : DataFrame
+        Raw data Proteome Discoverer corresponding proteins data.
     """
     def __init__(self, peptides_file, proteins_file):
         """Loads the raw data for peptides_file and proteins_file as DataFrames that are contained as attributes.
@@ -36,12 +36,13 @@ class RawData:
             Please make sure that your files are in your current working directory. If you are
             working in jupyter notebook please put the a copy of the peptides and proteins in
             the same directory as the notebook file.
+
         Parameters
         ----------
-            peptides_file : str
-                Name of Proteome Discoverer Peptides file as string.
-            proteins_file : str
-                Name of Proteome Discoverer Proteins file as string.
+        peptides_file : str
+            Name of Proteome Discoverer Peptides file as string.
+        proteins_file : str
+            Name of Proteome Discoverer Proteins file as string.
         """
         self.peptides = pd.read_csv(peptides_file, delimiter="\t", low_memory=False)
         print("number of peptides:", self.peptides.shape[0])
@@ -53,15 +54,15 @@ def manyModSel(pepdf, *terms):
 
     Parameters
     ----------
-        pepdf : DataFrame
-            With peptides information.
-        terms : str
-            Can be any number of modifictions as a string. Case does not matter and regex special characters can be
-            used e.g. 'acetyl', 'Phospho',hydroxy...methyl.glutaryl,'ect'
+    pepdf : DataFrame
+        With peptides information.
+    terms : str
+        Can be any number of modifictions as a string. Case does not matter and regex special characters can be
+        used e.g. 'acetyl', 'Phospho',hydroxy...methyl.glutaryl,'ect'
     Returns
     -------
-        selected : DataFrame or np.NAN
-            Contains the just the modified peptides.
+    selected : DataFrame or np.NAN
+        Contains the just the modified peptides.
     """
     for i in terms:
         moddex = pepdf.Modifications.str.contains(pat=i, case=False)
@@ -75,27 +76,58 @@ def manyModSel(pepdf, *terms):
 
 def normFactors(peptide_data):
     """Takes peptide abundance data and returns normalization factors.
-    """
-    return peptide_data.sum() / peptide_data.sum().mean()
 
+    Normalization factors are derived by the taking the sum of each column in DataFrame then dividing each sum by the
+    mean of all the sums.
+
+    Parameters
+    ----------
+    peptide_data : DataFrame
+
+    Returns
+    -------
+    norm_factors: DataFrame
+
+    """
+    norm_factors = peptide_data.sum() / peptide_data.sum().mean()
+    return norm_factors
 
 def normalizeTo(different, normal):
-    return different / normFactors(normal).as_matrix()
+    """Normalizes one DataFrame to another.
 
+    The 'different' DataFrame is normalized to the 'normal' DataFrame using the normFactors function.
+
+    Parameters
+    ----------
+    different : DataFrame
+    normal : DataFrame
+
+    Returns
+    -------
+    normalized : DataFrame
+    """
+    normalized = different / normFactors(normal).as_matrix()
+    return
 
 class Logger:
     """The class Logger preforms operations on normalized peptides.
 
-    Attributes:
-        log2 (:obj): DataFrame containing the log2 of the normalized data
-        ave (:obj): DataFrame containing the average of the aformentioned DataFrame
-        log_div_ave (:obj): DataFrame containing the log2-ave.
+    Attributes
+    ----------
+    log2 : DataFrame
+        Containing the log2 of the normalized data
+    ave : DataFrame
+        Containing the average of the aformentioned DataFrame
+    log_div_ave : DataFrame
+        Containing the log2-ave.
     """
 
     def __init__(self, normalized_data):
         """
-        Args:
-            normalized_data (:obj): Takes normalized peptide or protein in a DataFrame
+        Parameters
+        ----------
+        normalized_data : DataFrame
+            Takes normalized peptide or protein in a DataFrame
         """
         logged = normalized_data.apply(np.log2)
         logged.columns = "Log2 " + logged.columns
@@ -110,15 +142,21 @@ class Logger:
         log_div_ave.index = ave.index
         self.log_div_ave = log_div_ave
 
-
 def masterCleanse(protein_df):
     """Filters raw protein DataFrame for master proteins.
 
-    Args:
-        protein_df (:obj): Raw protein DataFrame
-    Return:
-        clean (:obj): Protein DataFrame that contains only proteins with 'IsMasterProtein'
-            in 'Master' column of protein_df
+    The raw protein data from Proteome Discoverer there is a column with the title 'Master' this funtion scans through
+    that column and selects only the proteins that end with the string "IsMasterProtein"
+
+    Parameters
+    ----------
+    protein_df : DataFrame
+        Raw protein DataFrame
+
+    Returns
+    -------
+    clean : DataFrame
+        Protein DataFrame that contains only proteins with 'IsMasterProtein' in 'Master' column of protein_df
     """
     clean = protein_df.ix[protein_df.Master.str.endswith("IsMasterProtein")]
     return clean
@@ -127,12 +165,17 @@ def masterCleanse(protein_df):
 def onePerQ(protein_df):
     """Filters raw protein DataFrame for proteins that are less than 1% the expected q-value.
 
-    Args:
-        protein_df (:obj): Raw protein DataFrame
+    Scans through the protein DataFrame selecting only the proteins with less than 1% of the expected q-value.
 
-    Return:
-        clean (:obj): Protein DataFrame that contains only proteins with proteins only less
-            than 1% of the expected q-value.
+    Parameters
+    ----------
+    protein_df : DataFrame
+        Raw protein DataFrame
+
+    Returns
+    -------
+    clean : DataFrame
+        Protein data that contains only proteins with proteins only less than 1% of the expected q-value.
     """
     one_per = protein_df["Exp. q-value"] < .01
     one_per = protein_df.ix[one_per]
@@ -140,15 +183,17 @@ def onePerQ(protein_df):
 
 
 def masterOne(protein_df):
-    """Takes a raw protein DataFrame and filters it using first the 'masterCleanse' function and then
-    the 'onePerQ' function.
+    """Takes a raw protein DataFrame and filters it using first the 'masterCleanse' function and 'onePerQ' function.
 
-    Args:
-        protein_df (:obj): Raw protein DataFrame
+    Parameters
+    ----------
+    protein_df : DataFrame
+        Raw proteins.
 
-    Return:
-        master_one (:obj): DataFrame of master proteins with exp. q-value <1%
-
+    Returns
+    -------
+    master_one : DataFrame
+        Of master proteins with exp. q-value <1%
     """
     master = masterCleanse(protein_df)
     master_one = onePerQ(master)
@@ -158,17 +203,19 @@ def masterOne(protein_df):
 def masterPep(peptide_df):
     """Takes a peptide DataFrame and returns just the first master protein accession for each peptide.
 
-    Note
-    ----
-    Peptides with no master protein accession will be lost however the index of peptide_df will be preseved.
+    Notes
+    -----
+    Assumes the first uniprot ID list is the correct one. Peptides with no master protein accession will be lost however
+    the index of peptide_df will be preserved.
 
     Parameters
     ----------
-    peptide_df:
+    peptide_df : DataFrame
 
     Returns
     -------
-    master_prot_acc:
+    master_prot_acc : DataFrame
+
     """
     master_prot_acc = [i.split(';')[0] for i in peptide_df['Master Protein Accessions'].dropna()]
 
@@ -178,26 +225,60 @@ def masterPep(peptide_df):
 
 class WithInput:
     """
+    Attributes
+    ----------
+    raw : DataFrame
+        The raw DataFrame with all information.
+    abundance : DataFrame
+        Abundance columns from raw DataFrame.
+    inputs : DataFrame
+        The input fraction from the abundance columns.
+    enriched : DataFrame
+        The enriched fraction from the abundance columns.
 
     """
     def __init__(self, raw):
-        """
+        """Takes raw DataFrame and isolates the abundance columns breaking those into the input and enriched fractions.
+
         Parameters
         ----------
-        raw
+        raw: DataFrame
+
         """
         self.raw = raw
         self.abundance = omin.sep(raw, 'Abundance:')
         self.inputs, self.enriched = omin.sepCon(self.abundance, 'Input')
 
 class Compare:
+    """
+    Attributes
+    ----------
+    abundance : DataFrame
+    lfc : DataFrame
+        Log2 Fold Change in the given comparison.
+    pval : DataFrame
+        P-values in the given comparison.
+    num_std : DataFrame
+        The standard deviation of the numerator in the comparison.
+    dem_std : DataFrame
+        The standard deviation of the denominator in the comparison.
+    num_ave : DataFrame
+        The mean of the columns in the numerator of the comparison.
+    dem_ave : DataFrame
+        The mean of the columns in the denominator of the comparison.
+
+    """
     def __init__(self, abundance, genotype_list):
         '''
 
         Parameters
         ----------
-        abundance
-        genotype_list
+        abundance : DataFrame
+            Abundance columns.
+        genotype_list : list
+            A list of the genotypes to be compared. The first element of the list is the numerator the second is the
+            denominator in the comparison.
+
         '''
         self.abundance = abundance
         # Set the numerator
@@ -218,10 +299,47 @@ class Compare:
         self.dem_ave = pd.DataFrame(dem.mean(axis=1), columns=[genotype_list[1] + " AVE"], index=dem.index)
 
 class ModDetect:
-    """
-    This class detects the modified peptides and proteins for a given modification.
+    """This class detects the modified peptides and proteins for a given modification.
+
+    Attributes
+    ----------
+    raw : DataFrame
+    mpa : DataFrame
+    fdr : DataFrame
+    fdr_mpa_prot : DataFrame
+        FDR filterd master protein accessions with the same index as the proteins DataFrame.
+    fdr_mpa_pep : DataFrame
+        FDR filterd master protein accessions with the same index as the peptides DataFrame.
+    correlated_protein_abundance : DataFrame
+    carta_prot : DataFrame
+    carta_pep : DataFrame
+    bdex : DataFrame
+        Just mitochondrial peptides.
+    wdex : DataFrame
+        Just the Non-mitochondrial proteins.
+    norm_abundance : DataFrame
+    relative_abundance : DataFrame
+    relative_occupancy : DataFrame
     """
     def __init__(self, whole_set, modification, genotypes, processed_peptides, processed_proteins, compare_in_order):
+        """
+
+        Parameters
+        ----------
+        whole_set : DataFrame
+            Set of all the peptides.
+        modification : str
+            The modification to detect.
+        genotypes : list
+            List of genotypes
+        processed_peptides : DataFrame
+            Normalized peptides.
+        processed_proteins : DataFrame
+            Normalized proteins.
+        compare_in_order : bool
+            True means the first element in the genotypes list the numerator in the comparison.
+
+        """
         # Grab the raw peptide data for modification in question
         self.raw = manyModSel(whole_set, modification)
         # Grab the master protein accession for the peptides
@@ -258,12 +376,35 @@ class ModDetect:
 
 class Experiment:
     """
-    Takes instance of
+
+    Attributes
+    ----------
+    peptides : (:obj)
+        WithInput object.
+    proteins : (:obj)
+        WithInput object.
+    [modifications] : (:obj)
+        These modification could be anything defined by the user. The defaults are Acetyl and Phospho
+
     """
     def __init__(self, raw_file, modifications=["Acetyl", "Phospho"], genotypes=["ko", "wt"], compare_in_order=True):
+        """
+
+        Parameters
+        ----------
+        raw_file : (:obj)
+            Takes RawData object
+        modifications : list
+            These can be defined by the user. If no list is entered then it defaults to ["Acetyl","Phospho"]
+        genotypes : list
+            These can be defined by the user. If no list is entered then it defaults to ["ko","wt"]
+        compare_in_order : bool
+            Should the genotypes be compared in their current order with the first element as the numerator and the
+            second as the denominator. If False the list is reversed. Defaults to True.
+        """
         self.peptides = WithInput(raw_file.peptides)
         self.proteins = WithInput(raw_file.proteins)
-        # FIXME: the next would probably be better handled as a outside fucntion or class idk
+        # FIXME: the next couple of lines would probably be better handled in an outside function or class idk.
         # Normalize the enriched peptides to the input peptides
         setattr(self.peptides, 'norm', normalizeTo(self.peptides.enriched, self.peptides.inputs))
         # Normlize the enriched proteins to the input peptides
@@ -274,9 +415,7 @@ class Experiment:
         setattr(self.proteins, 'norm_log', Logger(self.proteins.norm))
         # Grab the master proteins with <1% FDR
         setattr(self.proteins, 'fdr', masterOne(raw_file.proteins))
-
         fdr_mpa_list = pd.DataFrame(self.proteins.fdr.Accession, index=self.proteins.fdr.Accession.index)
-
         # Dynamically set the modifications as class attributes
         for mod in modifications:
             if type(manyModSel(raw_file.peptides, mod_dict[mod])) != type(np.nan):

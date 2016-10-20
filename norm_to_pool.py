38c0a4a62490a4546cc8520b4ae91dece65aca19
James Draper <james.draper@duke.edu>
2016-10-20 12:10:51 -0400

    Added functions.
    log2FC and ttester functions added.

--- norm_to_pool.py
+++ norm_to_pool.py
@@ -41,3 +41,56 @@ def normToPool(log2_div_ave):
     # Rename the columns to reflect the operations on them.
     lda_div_pool.columns = [re.sub("Log2-AVE", "Log2-AVE-Pool", i) for i in lda_div_pool.columns]
     return lda_div_pool
+
+###COMPARISON TOOLS###
+#======================================================================================================================
+
+def log2FC(numer, denom, new_column_name = ""):
+    """Takes the log2 fold change of normalized DataFrames of simillar size.
+
+    Parameters
+    ----------
+    numer : DataFrame
+        The numerator DataFrame
+    denom : DataFrame
+        The denominator DataFrame
+    new_column_name : str
+        Include your the name of your comparison here. Defaults to a blank string.
+
+    Returns
+    -------
+    lfc : DataFrame
+
+    """
+    if len(new_column_name) > 0:
+        new_column_name = " "+new_column_name
+    lfc = numer.mean(axis=1) - denom.mean(axis=1)
+    lfc = pd.DataFrame(lfc, columns=["LFC"+new_column_name],index=numer.index)
+    return lfc
+
+def ttester(numer, denom,new_column_name = ""):
+    """For pvalue comparision of types of DataFrames of similar shape.
+
+    Notes
+    -----
+    Make sure that your DataFrames are the same size. Future versions
+    Should rely on this method as a base for pvalue comparison.
+
+    Parameters
+    ----------
+    numer : DataFrame
+        The numerator DataFrame
+    denom : DataFrame
+        The denominator DataFrame
+
+    Returns
+    -------
+    pvals : DataFrame
+
+    """
+    from scipy.stats import ttest_ind
+    if len(new_column_name) > 0:
+        new_column_name = " "+new_column_name
+    pvals = ttest_ind(numer, denom, axis=1).pvalue
+    pvals = pd.DataFrame(pvals, columns=["pval"+new_column_name], index=numer.index)
+    return pvals

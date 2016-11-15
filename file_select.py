f1153962fac0fab00156d29594bdd801c01a8ebe
James Draper <james.draper@duke.edu>
2016-11-15 12:02:52 -0500

    Hey look a main window!

--- file_select.py
+++ file_select.py
@@ -2,47 +2,61 @@ import sys
 from math import *
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+import modulocator
+from modulocator import modulocator
+modulocator("Notebook")
+import omin
 
 class FileSelect(QDialog):
 
     def __init__(self, parent=None):
-        super(FileSelect, self).__init__(parent)
-        #Create QLineEdit boxes for peptides_path and protiens_path
-        self.peptides_path = QLineEdit("Peptides file")
-        self.protiens_path = QLineEdit("Protiens file")
 
+        super(FileSelect, self).__init__(parent)
+        #BUTTONS
+        self.peptides_path = QLineEdit("Peptides file")
+        self.proteins_path = QLineEdit("Proteins file")
         self.peptide_browse_button = QPushButton("Browse")
-        self.protien_browse_button = QPushButton("Browse")
-        self.next_button = QPushButton("Next")
-        self.back_button = QPushButton("Back")
+        self.protein_browse_button = QPushButton("Browse")
+        # self.next_button = QPushButton("Next")
+        # self.back_button = QPushButton("Back")
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Ok|QDialogButtonBox.Cancel)
+        #LABELS
         self.testlabel = QLabel("")
         # self.peptides_path.selectAll()
+
+        #LAYOUT
         grid = QGridLayout()
         grid.addWidget(self.peptides_path,0,0)
         grid.addWidget(self.peptide_browse_button,0,1)
-        grid.addWidget(self.protiens_path,1,0)
-        grid.addWidget(self.protien_browse_button,1,1)
-        grid.addWidget(self.next_button,2,1)
-        grid.addWidget(self.back_button,2,0)
+        grid.addWidget(self.proteins_path,1,0)
+        grid.addWidget(self.protein_browse_button,1,1)
+        # grid.addWidget(self.next_button,2,1)
+        # grid.addWidget(self.back_button,2,0)
         grid.addWidget(self.testlabel,3,0)
-
+        grid.addWidget(self.buttonBox,4,1)
         self.setLayout(grid)
         self.peptides_path.setFocus()
-        # self.connect(self.peptide_browse_button, SIGNAL("returnPressed()"),self.updateUi)
+        #CONNECTIONS
         self.connect(self.peptide_browse_button, SIGNAL("clicked()"), lambda: self.setPath("peptides"))
-        self.connect(self.protien_browse_button,SIGNAL("clicked()"),lambda: self.setPath("protiens"))
-        self.connect(self.next_button,SIGNAL("clicked()"),self.testLabelChange)
+        self.connect(self.protein_browse_button, SIGNAL("clicked()"), lambda: self.setPath("proteins"))
+        self.connect(self.buttonBox,SIGNAL("accepted()"),self.accept)
+        self.connect(self.buttonBox,SIGNAL("rejected()"),self.reject)
+        #WINDOW TITLE
         self.setWindowTitle("Select Files")
 
     def setPath(self,tool):
-        path = QFileDialog.getExistingDirectory(self, "Select File")# self.__dict__[tool+"_path"])
+        # path = QFileDialog.getExistingDirectory(self, "Select File")
+        path = QFileDialog.getOpenFileName(self, "Select File")
         if path:
             self.__dict__[tool+"_path"].setText(QDir.toNativeSeparators(path))
 
     def testLabelChange(self):
-        self.testlabel.setText(self.peptides_path.text())
+        data = omin.RawData(self.peptides_path.text(),self.proteins_path.text()).peptides.shape
+        self.testlabel.setText("Peptide rows:"+str(data[0]))
 
-app = QApplication(sys.argv)
-form = FileSelect()
-form.show()
-app.exec_()
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    form = FileSelect()
+    form.show()
+    app.exec_()
---
+++ main.py
@@ -0,0 +1,132 @@
+import os
+import platform
+import stat
+import sys
+
+import modulocator
+from modulocator import modulocator
+modulocator("Notebook")
+import omin
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import file_select
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__(None)
+        self.filename = None
+        self.peptides_path = None
+        self.proteins_path = None
+        self.omin_object = None
+        #Set window TITLE
+        self.setWindowTitle("omin-omics analytics")
+
+        # self.image = QImage()
+        # self.imageLabel = QLabel("Hello")
+        # self.imageLabel.setMinimumSize(200, 200)
+        # self.imageLabel.setAlignment(Qt.AlignCenter)
+        # self.imageLabel.setContextMenuPolicy(Qt.ActionsContextMenu)
+        # self.setCentralWidget(self.imageLabel)
+
+        treeLabel = QLabel("Tre&e")
+        self.treeWidget = QTreeWidget()
+        treeLabel.setBuddy(self.treeWidget)
+        self.setCentralWidget(self.treeWidget)
+        #TABLE WIDGET
+        tableDockWidget = QDockWidget("Table",self)
+        tableDockWidget.setObjectName("TableDockWidget")
+        tableDockWidget.setAllowedAreas(Qt.LeftDockWidgetArea|Qt.RightDockWidgetArea)
+        self.tableWidget = QTableWidget()
+        tableDockWidget.setWidget(self.tableWidget)
+        self.addDockWidget(Qt.RightDockWidgetArea,tableDockWidget)
+        #LOG WIDGET
+        logDockWidget = QDockWidget("Log", self)
+        logDockWidget.setObjectName("LogDockWidget")
+        logDockWidget.setAllowedAreas(Qt.LeftDockWidgetArea|Qt.RightDockWidgetArea|Qt.TopDockWidgetArea|Qt.BottomDockWidgetArea)
+        self.listWidget = QListWidget()
+        logDockWidget.setWidget(self.listWidget)
+        self.addDockWidget(Qt.BottomDockWidgetArea, logDockWidget)
+
+        fileNewAction = self.createAction("&New...",self.fileNew,QKeySequence.New, "filenew", "Create an image file")
+
+        self.fileMenu = self.menuBar().addMenu("&File")
+        self.fileMenuActions = (None, fileNewAction)
+        self.connect(self.fileMenu, SIGNAL("aboutToShow()"), self.updateFileMenu)
+        self.populateTree()
+        self.populateTable()
+        # self.connect(self.treeWidget.currentItem(),SIGNAL('selectionChanged()'),self.updateSatus("hey"))
+        # self.fileMenuActions.addActions(self.fileMenu,self.fileMenuActions)
+
+    def createAction(self, text, slot=None, shortcut=None, icon=None,
+                     tip=None, checkable=False, signal="triggered()"):
+        action = QAction(text, self)
+        if icon is not None:
+            action.setIcon(QIcon(":/{}.png".format(icon)))
+        if shortcut is not None:
+            action.setShortcut(shortcut)
+        if tip is not None:
+            action.setToolTip(tip)
+            action.setStatusTip(tip)
+        if slot is not None:
+            self.connect(action, SIGNAL(signal), slot)
+        if checkable:
+            action.setCheckable(True)
+        return action
+
+    def fileNew(self):
+        dialog = file_select.FileSelect(self)
+        dialog.show()
+        if dialog.exec_():
+            self.peptides_path = dialog.peptides_path.text()
+            self.proteins_path = dialog.proteins_path.text()
+            self.omin_object = omin.RawData(self.peptides_path,self.proteins_path)
+            self.updateSatus("omin project loaded.")
+            # self.imageLabel.setText(self.peptides_path)
+
+    def addActions(self, target, actions):
+        for action in actions:
+            if action is None:
+                target.addSeparator()
+            else:
+                target.addAction(action)
+
+    def updateSatus(self,message):
+        self.listWidget.addItem(message)
+        self.populateTree()
+
+    def updateFileMenu(self):
+        self.fileMenu.clear()
+        self.addActions(self.fileMenu, self.fileMenuActions)
+
+    def populateTree(self):
+        self.treeWidget.clear()
+        self.treeWidget.setHeaderLabels(["project"])
+        if self.omin_object is not None:
+            # return
+            att_list = self.omin_object.__dict__.keys()
+            for att in att_list:
+                QTreeWidgetItem(self.treeWidget, [att])
+        else:
+            att_list = ["None Open"]
+            for att in att_list:
+                QTreeWidgetItem(self.treeWidget, [att])
+
+    def populateTable(self):
+        selected = None
+        self.tableWidget.clear()
+
+    def treeSelect(self):
+        term = self.treeWidget.currentItem()
+        self.updateSatus(term)
+
+
+def main():
+    app = QApplication(sys.argv)
+    # app.setWindowIcon(QIcon(":/icon.png"))
+    main = MainWindow()
+    main.show()
+    app.exec_()
+
+main()

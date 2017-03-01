f19cbdf9f62159c359b87b407a47b30d202d6898
James Draper <james.draper@duke.edu>
2017-03-01 17:04:14 -0500

    bhFDR improved handling of missing data

--- stats/fdr.py
+++ stats/fdr.py
@@ -1,36 +1,15 @@
-# This code has been adapted from Bonferioni Calculator v1.1.py here is the original documentation:
-    #Source code for “Bonferroni Calculator”
-    #program requires python 3.2 or higher, available for free at http://www.python.org/getit/releases/3.2/
-    #Contact Dr. Naugler at:
-    #C414, Diagnostic and Scientific Centre
-    #14, 3535 Research Road NW
-    #Calgary AB Canada T2L 2K8
-    #Ph: 403-770-3756; Email: christopher.naugler@cls.ab.ca
-    #NOTE Version 1.1, released March 2013, contains an important fix for the interpretation of Bonferroni-Holm and Benjamini-Hochberg calculations
-    #Specifically, in version 1.1, when Benjamini-Hochberg calculations are performed in the rank-ordered list of P-values AFTER the first significant
-    #P-value has been declared, any subsequent P-values which calculate to >0.05 are defaulted to equal the largest statistically significant adjusted P-value
-    #The resulting calculations are then identical to Benjamini-Hochberg corrections performed with the statistical program "R"
-    #
-    #For Bonferroni-Holm corrections, version 1.1 also contains an important fix.
-    #Specifically, when Bonferroni-Holm corrections are performed in the rank-ordered list of P-values, calculations are stopped after the first non-significant
-    #P-value has been declared, and subsequent P-values are defaulted to P=1
-    #
-    #Version 1.1 also adds the ability to import the list of starting P-values from a text file instead of typing them individually into the program
-    #Version 1.1 also adds the interpretation "significant" and "non-significant" after all corrected p-values
-    #
-    #We wish to thank Nicholas Wiebe, MSc for help in coding these changes
-    #
-    #Please update to this latest version
-    #Information about future updates can be found at: https://sites.google.com/site/christophernaugler/open-source-software-for-performing-bonferroni-and-related-corrections
-    #The authors do not assume any liability for errors resulting from the use of this software.
 import pandas as pd
 
 class Comparison:
+    """
+    This code has been adapted from Bonferioni Calculator v1.1.py. For the full
+    version visit the following website:
+    https://sites.google.com/site/christophernaugler/open-source-software-for-performing-bonferroni-and-related-corrections
+    """
     def __init__(self, entry_number, p_value):
         self.entry_number = entry_number
         self.p_value = p_value
         self.p_corrected = 0
-        # version 1.1 added new class variable do designate a non-significant value
         self.significant = 0
 
     def __repr__(self):
@@ -38,9 +17,14 @@ class Comparison:
 
 
 def benjamini_hochberg(comparison_objects, sig_level):
-    # Sorts by p-value
+    """
+    This code has been adapted from Bonferioni Calculator v1.1.py. For the full
+    version visit the following website:
+    https://sites.google.com/site/christophernaugler/open-source-software-for-performing-bonferroni-and-related-corrections
+    """
 
-    sequential_comparison_objects = sorted(comparison_objects, key=lambda comparison_value: comparison_value.p_value,
+    sequential_comparison_objects = sorted(comparison_objects,
+                                           key=lambda comparison_value: comparison_value.p_value,
                                            reverse=True)
     rank = 1
     total_measurements = len(comparison_objects)
@@ -79,26 +63,45 @@ def benjamini_hochberg(comparison_objects, sig_level):
 def bhFDR(pval_series):
     """Returns the p-adjusted values for a series of p-values.
 
-    Note
-    ----
-    Make sure your p-values Series contain NO NaNs. If you DataFrame contains NaNs the p-adjusted values will be
-    incorrect.
-
     Parameters
     ----------
     pval_series : Series
-        Series must contain floats with no NaN values.
 
     Returns
     -------
     p_adjust : DataFrame
 
     """
+    # Test to see if pval_series is actually a DataFrame.
+    if type(pval_series) == pd.core.frame.DataFrame:
+        # If it is a DataFrame test to make sure that it only has one column.
+        if pval_series.shape[1] == 1:
+            # If it does has just one columns slice it from the DataFrame to create a Series.
+            pval_series = pval_series.ix[:,0]
+        else:
+            print("Please format your p-values as a pandas series and retry.")
+
+    # Test to see if pval_series is Series.
+    if type(pval_series) == pd.core.series.Series:
+        # Test for NANs
+        if pval_series.isnull().any():
+            og_pval = pval_series
+            pval_series = pval_series.dropna()
+
     comparison_objects = []
     for n, v in enumerate(pval_series):
+        # Create and append comparison objects for each value.
         comparison_objects.append(Comparison(n, v))
+
+    # Finally run through BH correction.
     bhc = benjamini_hochberg(comparison_objects, .05)
     p_adjust = pd.DataFrame([bhc[i].p_corrected for i in range(len(bhc))],
                             columns=["p-adjusted"],
                             index=pval_series.index)
+
+    # Test to see if og_pval exists in local namespace
+    if "og_pval" in locals():
+        # If it does then reindex p_adjust to og_pval.index
+        p_adjust = p_adjust.reindex(index=og_pval.index)
+
     return p_adjust

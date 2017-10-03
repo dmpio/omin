1ab48feaeb820ac12f0aa24f3d364062f3bceccb
James Draper <james.draper@duke.edu>
2017-10-03 14:07:34 -0400

    fixed conda-forge installs

--- omin_win7_installer.cmd
+++ omin_win7_installer.cmd
@@ -1,37 +1,37 @@
-REM Upgrade conda if needed.
-conda upgrade -y conda 
+@REM Upgrade conda if needed.
+conda upgrade -y conda
 
-REM Uninstall any old verision of omin.
+@REM Uninstall any old verision of omin.
 pip uninstall -y omin
 
-REM These dependencies need to be installed via conda.
-conda install -cy conda-forge ipywidgets
-conda install -y bokeh
-conda install -cy conda-forge jupyter_contrib_nbextensions
-conda install -cy conda-forge jupyter_nbextensions_configurator
+@REM These dependencies need to be installed via conda.
+conda install -c conda-forge ipywidgets -y
+conda install -c conda-forge bokeh -y
+conda install -c conda-forge jupyter_contrib_nbextensions -y
+conda install -c conda-forge jupyter_nbextensions_configurator -y
 
-REM Turn on hinterland.
+@REM Turn on hinterland.
 jupyter nbextension enable hinterland/hinterland
 
-REM Make sure ipywidgets is turned on.
+@REM Make sure ipywidgets is turned on.
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
-REM Create jupyter notebook config file.
+@REM Create jupyter notebook config file.
 jupyter notebook -y --generate-config
 
-REM Install omin.
+@REM Install omin.
 python setup.py install
 
-REM omin install files left behind.
+@REM omin install files left behind.
 rmdir build /s /q
 rmdir dist /s /q
 rmdir omin.egg-info /s /q
 
-REM Create a standard JupyterNotebooks dir.
+@REM Create a standard JupyterNotebooks dir.
 cd %userprofile%\Documents
 mkdir JupyterNotebooks
 
-REM Add lines to .jupyter/jupyter_notebook_config.py file so jupyter notebook always opens to the new default dir.
+@REM Add lines to .jupyter/jupyter_notebook_config.py file so jupyter notebook always opens to the new default dir.
 cd %userprofile%\.jupyter
 (echo. && echo # Make jupyter notebook open in ~\Documents\JupyterNotebook && echo import os) > scratch.txt
 

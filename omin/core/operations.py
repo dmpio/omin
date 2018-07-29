35901b4163b2220c432f9d4e737fc88ac503f925
James Draper <james.draper@duke.edu>
2018-07-29 11:01:43 -0400

    now with top-level start method

--- omin/core/operations.py
+++ omin/core/operations.py
@@ -13,6 +13,7 @@ Provides operations and possible a main class entry point for the CLI.
 # ----------------
 import os
 import pickle
+from glob import glob
 
 # ----------------
 # INTERNAL IMPORTS
@@ -72,26 +73,43 @@ class Operate(object):
     @classmethod
     def create_or_load_process(cls, process_path, overwrite=None, **kwargs):
 
+        # If the process file already exists...
         if os.path.exists(process_path):
+            # Collect user input...
             if overwrite is None:
                 message = """A process with the same name exits in this directory.
                 Would you like to:
                 1)Load the file
                 2)Overwrite the file
                 """
-
                 overwrite = input(message)
                 overwrite = int(overwrite)
                 overwrite = bool(overwrite-1)
 
+            # If overwrite is True then overwrite the file.
             if overwrite:
                 proc = Process(**kwargs)
                 cls.create_process(proc, process_path, usr_inp="Y")
+
+            # Exit without over writting.
             else:
                 proc = cls.load_process(process_path)
 
             return proc
+        # The path DNE the process file is witten.
         else:
             proc = Process(**kwargs)
             cls.create_process(proc, process_path)
             return proc
+
+    @classmethod
+    def start(cls, file_list=None, rescue_entrez_ids=True, *args, **kwargs):
+        if file_list is None:
+            # FIXME: Open this very narrow edge case up.
+            # FIXME: Add error messages.
+            file_list = glob("Proteome_Discoverer_Results/*.bz2")
+
+        cls.create_or_load_process(file_list=file_list,
+                                   rescue_entrez_ids=rescue_entrez_ids,
+                                   *args,
+                                   **kwargs)
--- omin/visualize/utils.py
+++ omin/visualize/utils.py
@@ -7,6 +7,8 @@
 
 import os
 import numpy as np
+from pandomics import pandas as pd
+
 from ..utils import IOTools
 
 try:
@@ -29,3 +31,73 @@ def scatter_scale(target, max_size=50.0):
     """Return a scaled list of sizes."""
     result = target.apply(lambda x:max_size*(1-x))
     return result
+
+
+def volcano_latex(title=None, ax=None, *args, **kwargs):
+    """Returns matplotlib plot with latex labels."""
+
+    if ax is None:
+        ax = plt.gca()
+
+    # f, ax = plt.subplots(1, 1, figsize=(10,5))
+
+    latex_title = '$\mathrm{{{}}}$'.format(title)
+    # plt.title(latex_title, fontsize=18)
+    ax.set_title(latex_title, fontsize=18)
+
+    latex_xlabel = '$\mathrm{Log_{2}FC}$'
+    # plt.xlabel(latex_xlabel, fontsize=16)
+    ax.set_xlabel(latex_xlabel, fontsize=16)
+
+    latex_ylabel = '$\mathrm{Log_{10}(p value)}$'
+    # plt.ylabel(latex_ylabel, fontsize=16)
+    ax.set_ylabel(latex_ylabel, fontsize=16)
+
+    return ax.plot(*args, **kwargs)
+
+
+def compartmentalize(boolean_series,
+                     true_face_color=None,
+                     true_edge_color=None,
+                     false_face_color=None,
+                     false_edge_color=None,
+                     fc_name="face_color",
+                     ec_name="edge_color",
+                     axis=1):
+
+    """Returns a DataFrame with the folloing columns ..."""
+
+    if true_face_color is None:
+        true_face_color = [.19, .15, .11, .9]
+
+    if true_edge_color is None:
+        true_edge_color = [.6, .25, .25, .25]
+
+    if false_face_color is None:
+        false_face_color = [.97, .981, .9, .50]
+
+    if false_edge_color is None:
+        false_edge_color = [.35, .35, .35, .8]
+
+    face_color_series = boolean_color(boolean_series, true_face_color, false_face_color, name=fc_name)
+    edge_color_series = boolean_color(boolean_series, true_edge_color, false_edge_color, name=ec_name)
+
+    results = concat([face_color_series, edge_color_series], axis=axis)
+
+    return results
+
+def compartmentalize_and_scale(boolean_series,
+                               scale_series,
+                               max_size=50,
+                               axis=1,
+                               **kwargs):
+    """Returns DataFrame for volcano plots."""
+
+    compartment = compartmentalize(boolean_series=boolean_series, **kwargs)
+    scalar = scatter_scale(scale_series, max_size=max_size)
+    results = pd.concat([compartment, scalar.rename('scalar')], axis=axis)
+    return results
+
+
+def mitocarta_scale(dataframe):
+    return compartmentalize_and_scale(dataframe["MitoCarta2_List"], dataframe["p_adjusted"])
